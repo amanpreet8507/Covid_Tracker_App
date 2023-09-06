734fd6153e7225f7f9fc77db6637eec6ed4c4b80
amanpreet8507 <105711554+amanpreet8507@users.noreply.github.com>
2023-09-06 13:45:50 -0700

    ref #4 error fix, card 1,2,3 done

--- main.js
+++ main.js
@@ -16,87 +16,80 @@ const APIKEY = "https://api.covidtracking.com/v1/us/daily.json";
 //     });
 // };
 
+async function fethCovidData() {
+  let covidData = await fetch(APIKEY);
+  let dataWithJSON = await covidData.json();
+  let finalOutputArray = dataWithJSON;
+  console.log(finalOutputArray);
 
-
-async function fethCovidData(){
-    let covidData = await fetch(APIKEY);
-    let dataWithJSON = await covidData.json();
-    let finalOutputArray = dataWithJSON;
-    console.log(finalOutputArray);
-   
-    createCardElement1(finalOutputArray);
+  createCardElement1(finalOutputArray);
 }
 
-function createCardElement1(finalOutputArray){
-    let container1 = document.querySelector('#maincontainer');
+function createCardElement1(finalOutputArray) {
+  let container1 = document.querySelector("#maincontainer");
 
-    let dataDiv = document.createElement("div");
-    dataDiv.classList.add("datadiv");
+  let dataDiv = document.createElement("div");
+  dataDiv.classList.add("datadiv");
 
-    let dataDiv2 = document.createElement("div")
-    dataDiv2.classList.add("datadiv2");
+  let dataDiv2 = document.createElement("div");
+  dataDiv2.classList.add("datadiv2");
 
-    //Div1
-        let firstCard = document.createElement("div");
-        firstCard.classList.add("div1");
-        //Heading(Number of States)
-        let firstCardHeading = document.createElement("h3");
-        firstCardHeading.textContent =  "Number of States";
-        firstCardHeading.classList.add("heading");
+  //Div1
+  let firstCard = document.createElement("div");
+  firstCard.classList.add("div1");
+  //Heading(Number of States)
+  let firstCardHeading = document.createElement("h3");
+  firstCardHeading.textContent = "Number of States";
+  firstCardHeading.classList.add("heading");
 
-        firstCard.appendChild(firstCardHeading);
-        //Number of States Data fething from API
-        let numOfStates = document.createElement("h1");
-        numOfStates.textContent = finalOutputArray[0].states;
-        numOfStates.classList.add("numberData");
+  firstCard.appendChild(firstCardHeading);
+  //Number of States Data fething from API
+  let numOfStates = document.createElement("h1");
+  numOfStates.textContent = finalOutputArray[0].states;
+  numOfStates.classList.add("numberData");
 
-        firstCard.appendChild(numOfStates);
-        
-        
-    //Div2
-        let secondCard= document.createElement("div");
-        secondCard.classList.add("div2");
-        //Heading(Negative and positive patients)
-        let secondCardHeading = document.createElement("h3");
-        secondCardHeading.textContent = "Positive Cases";
-        secondCardHeading.classList.add("heading");
+  firstCard.appendChild(numOfStates);
 
-        secondCard.appendChild(secondCardHeading);
+  //Div2
+  let secondCard = document.createElement("div");
+  secondCard.classList.add("div2");
+  //Heading(Negative and positive patients)
+  let secondCardHeading = document.createElement("h3");
+  secondCardHeading.textContent = "Positive Cases";
+  secondCardHeading.classList.add("heading");
 
-        //Number of States Data getting from API
-        let numOfPositiveCases = document.createElement("h1");
-        numOfPositiveCases.textContent = finalOutputArray[0].positive;
-        numOfPositiveCases.classList.add("numberData");
+  secondCard.appendChild(secondCardHeading);
 
-        secondDiv.appendChild(numOfPositiveCases);
-    
-    //Div3
-        let thirdCard = document.createElement("div");
-        thirdCard.classList.add("div3");
-        //Heading(Deceased)
-        let thirdCardHeading = document.createElement("h3");
-        thirdCardHeading.textContent = "Deceased";
-        thirdCardHeading.classList.add("heading");
+  //Number of States Data getting from API
+  let numOfPositiveCases = document.createElement("h1");
+  numOfPositiveCases.textContent = finalOutputArray[0].positive;
+  numOfPositiveCases.classList.add("numberData");
 
-        thirdCard.appendChild(thirdCardHeading);
+  secondCard.appendChild(numOfPositiveCases);
 
-        //Number of Deceased cases getting from API
-        let numOfDeaths = document.createElement("h1");
-        numOfDeaths.textContent = finalOutputArray[0].death;
-        numOfDeaths.classList.add("numberData");
+  //Div3
+  let thirdCard = document.createElement("div");
+  thirdCard.classList.add("div3");
+  //Heading(Deceased)
+  let thirdCardHeading = document.createElement("h3");
+  thirdCardHeading.textContent = "Deceased";
+  thirdCardHeading.classList.add("heading");
 
-        thirdCard.appendChild(numOfDeaths);
+  thirdCard.appendChild(thirdCardHeading);
 
+  //Number of Deceased cases getting from API
+  let numOfDeaths = document.createElement("h1");
+  numOfDeaths.textContent = finalOutputArray[0].death;
+  numOfDeaths.classList.add("numberData");
 
-        
-    
-    dataDiv.appendChild(firstDiv);
-    dataDiv.appendChild(secondDiv);
-    dataDiv2.appendChild(thirdDiv);
-    
+  thirdCard.appendChild(numOfDeaths);
 
-    container1.appendChild(dataDiv);
-    
+  dataDiv.appendChild(firstCard);
+  dataDiv.appendChild(secondCard);
+  dataDiv2.appendChild(thirdCard);
+
+  container1.appendChild(dataDiv);
+  container1.appendChild(dataDiv2);
 }
 
 //getDataFromSource();
